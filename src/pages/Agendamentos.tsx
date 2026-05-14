5e8bcf4bf5ad833b935e28f2683480ca97da2d50
henrrynho <henrrynho1902@gmail.com>
2026-05-14 19:44:25 -0300

    Update Agendamentos.tsx

--- src/pages/Agendamentos.tsx
+++ src/pages/Agendamentos.tsx
@@ -1,3 +1,4 @@
+import { criarNotaFiscalRascunho } from "@/services/notaFiscalService";
 import { useEffect, useState, useMemo } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
